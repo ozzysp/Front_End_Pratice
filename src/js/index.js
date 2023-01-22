c4d3695f8f8c58617f33f0899e7048fd15a08050
Ozzy Scuba <ozzysp@icloud.com>
2023-01-22 00:47:22 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,20 @@
-console.log('Show the document', document);
-console.log(document.querySelector(".trailer-btn"));
 const trailerBtn  = document.querySelector(".trailer-btn");
-trailerBtn.addEventListener("click", () => { 
-    modal.classList.add("opened")
-});
-const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-modal");
-closeModal.addEventListener("click", () => {modal.classList.remove("opened");
-video.setAttribute("src", "")
-})
-
 const video = document.getElementById("video");
+const modal = document.querySelector("video");
+const videoLink = video.scroll;
+
+function alternateModal() {
+    modal.classList.toggle("opened");
+}
+
+
+trailerBtn.addEventListener("click", () => { 
+    alternateModal();
+    video.setAttribute("src" , videoLink);
+});
+
+closeModal.addEventListener("click", () => {
+    alternateModal();
+    video.setAttribute("src", "");
+});
